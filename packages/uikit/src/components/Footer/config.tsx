e0d9d9895da225f4223cc1bbe3d035179047c582
Elvolution <75731970+Elvolution20@users.noreply.github.com>
2023-06-23 11:11:15 +0100

    Revert "Update on Footer"
    This reverts commit 4cf83b5881ae200b05971549d09fdd32c4b00c14.

--- packages/uikit/src/components/Footer/config.tsx
+++ packages/uikit/src/components/Footer/config.tsx
@@ -21,24 +21,24 @@ export const footerLinks: FooterLinkType[] = [
       },
       {
         label: "Blog",
-        href: "https://medium.com/zodiacswap",
+        href: "https://medium.com/pancakeswap",
       },
       {
         label: "Community",
         href: "https://docs.zodiacswap.xyz/contact-us/telegram",
       },
-      // {
-      //   label: "CAKE",
-      //   href: "https://docs.zodiacswap.xyz/tokenomics/cake",
-      // },
-      // {
-      //   label: "—",
-      // },
-      // {
-      //   label: "Online Store",
-      //   href: "https://pancakeswap.creator-spring.com/",
-      //   isHighlighted: true,
-      // },
+      {
+        label: "CAKE",
+        href: "https://docs.zodiacswap.xyz/tokenomics/cake",
+      },
+      {
+        label: "—",
+      },
+      {
+        label: "Online Store",
+        href: "https://pancakeswap.creator-spring.com/",
+        isHighlighted: true,
+      },
     ],
   },
   {
@@ -48,149 +48,149 @@ export const footerLinks: FooterLinkType[] = [
         label: "Customer",
         href: "Support https://docs.zodiacswap.xyz/contact-us/customer-support",
       },
-      // {
-      //   label: "Troubleshooting",
-      //   href: "https://docs.zodiacswap.xyz/help/troubleshooting",
-      // },
+      {
+        label: "Troubleshooting",
+        href: "https://docs.zodiacswap.xyz/help/troubleshooting",
+      },
       {
         label: "Guides",
         href: "https://docs.zodiacswap.xyz/get-started",
       },
     ],
   },
-  // {
-  //   label: "Developers",
-  //   items: [
-  //     {
-  //       label: "Github",
-  //       href: "https://github.com/pancakeswap",
-  //     },
-  //     {
-  //       label: "Documentation",
-  //       href: "https://docs.zodiacswap.xyz",
-  //     },
-  //     {
-  //       label: "Bug Bounty",
-  //       href: "https://app.gitbook.com/@pancakeswap-1/s/pancakeswap/code/bug-bounty",
-  //     },
-  //     {
-  //       label: "Audits",
-  //       href: "https://docs.zodiacswap.xyz/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited",
-  //     },
-  //     {
-  //       label: "Careers",
-  //       href: "https://docs.zodiacswap.xyz/hiring/become-a-chef",
-  //     },
-  //   ],
-  // },
+  {
+    label: "Developers",
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/pancakeswap",
+      },
+      {
+        label: "Documentation",
+        href: "https://docs.zodiacswap.xyz",
+      },
+      {
+        label: "Bug Bounty",
+        href: "https://app.gitbook.com/@pancakeswap-1/s/pancakeswap/code/bug-bounty",
+      },
+      {
+        label: "Audits",
+        href: "https://docs.zodiacswap.xyz/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited",
+      },
+      {
+        label: "Careers",
+        href: "https://docs.zodiacswap.xyz/hiring/become-a-chef",
+      },
+    ],
+  },
 ];
 
 export const socials = [
   {
     label: "Twitter",
     icon: TwitterIcon,
-    href: "https://twitter.com/zodiacswap",
+    href: "https://twitter.com/pancakeswap",
   },
-  // {
-  //   label: "Telegram",
-  //   icon: TelegramIcon,
-  //   items: [
-  //     {
-  //       label: "English",
-  //       href: "https://t.me/pancakeswap",
-  //     },
-  //     {
-  //       label: "Bahasa Indonesia",
-  //       href: "https://t.me/PancakeSwapIndonesia",
-  //     },
-  //     {
-  //       label: "中文",
-  //       href: "https://t.me/PancakeSwap_CN",
-  //     },
-  //     {
-  //       label: "Tiếng Việt",
-  //       href: "https://t.me/PancakeSwapVN",
-  //     },
-  //     {
-  //       label: "Italiano",
-  //       href: "https://t.me/pancakeswap_ita",
-  //     },
-  //     {
-  //       label: "русский",
-  //       href: "https://t.me/pancakeswap_ru",
-  //     },
-  //     {
-  //       label: "Türkiye",
-  //       href: "https://t.me/pancakeswapturkiye",
-  //     },
-  //     {
-  //       label: "Português",
-  //       href: "https://t.me/PancakeSwapPortuguese",
-  //     },
-  //     {
-  //       label: "Español",
-  //       href: "https://t.me/PancakeswapEs",
-  //     },
-  //     {
-  //       label: "日本語",
-  //       href: "https://t.me/pancakeswapjp",
-  //     },
-  //     {
-  //       label: "Français",
-  //       href: "https://t.me/pancakeswapfr",
-  //     },
-  //     {
-  //       label: "Deutsch",
-  //       href: "https://t.me/PancakeSwap_DE",
-  //     },
-  //     {
-  //       label: "Filipino",
-  //       href: "https://t.me/Pancakeswap_Ph",
-  //     },
-  //     {
-  //       label: "ქართული ენა",
-  //       href: "https://t.me/PancakeSwapGeorgia",
-  //     },
-  //     {
-  //       label: "हिन्दी",
-  //       href: "https://t.me/PancakeSwapINDIA",
-  //     },
-  //     {
-  //       label: "Announcements",
-  //       href: "https://t.me/PancakeSwapAnn",
-  //     },
-  //   ],
-  // },
-  // {
-  //   label: "Reddit",
-  //   icon: RedditIcon,
-  //   href: "https://reddit.com/r/pancakeswap",
-  // },
-  // {
-  //   label: "Instagram",
-  //   icon: InstagramIcon,
-  //   href: "https://instagram.com/pancakeswap_official",
-  // },
-  // {
-  //   label: "Github",
-  //   icon: GithubIcon,
-  //   href: "https://github.com/pancakeswap/",
-  // },
+  {
+    label: "Telegram",
+    icon: TelegramIcon,
+    items: [
+      {
+        label: "English",
+        href: "https://t.me/pancakeswap",
+      },
+      {
+        label: "Bahasa Indonesia",
+        href: "https://t.me/PancakeSwapIndonesia",
+      },
+      {
+        label: "中文",
+        href: "https://t.me/PancakeSwap_CN",
+      },
+      {
+        label: "Tiếng Việt",
+        href: "https://t.me/PancakeSwapVN",
+      },
+      {
+        label: "Italiano",
+        href: "https://t.me/pancakeswap_ita",
+      },
+      {
+        label: "русский",
+        href: "https://t.me/pancakeswap_ru",
+      },
+      {
+        label: "Türkiye",
+        href: "https://t.me/pancakeswapturkiye",
+      },
+      {
+        label: "Português",
+        href: "https://t.me/PancakeSwapPortuguese",
+      },
+      {
+        label: "Español",
+        href: "https://t.me/PancakeswapEs",
+      },
+      {
+        label: "日本語",
+        href: "https://t.me/pancakeswapjp",
+      },
+      {
+        label: "Français",
+        href: "https://t.me/pancakeswapfr",
+      },
+      {
+        label: "Deutsch",
+        href: "https://t.me/PancakeSwap_DE",
+      },
+      {
+        label: "Filipino",
+        href: "https://t.me/Pancakeswap_Ph",
+      },
+      {
+        label: "ქართული ენა",
+        href: "https://t.me/PancakeSwapGeorgia",
+      },
+      {
+        label: "हिन्दी",
+        href: "https://t.me/PancakeSwapINDIA",
+      },
+      {
+        label: "Announcements",
+        href: "https://t.me/PancakeSwapAnn",
+      },
+    ],
+  },
+  {
+    label: "Reddit",
+    icon: RedditIcon,
+    href: "https://reddit.com/r/pancakeswap",
+  },
+  {
+    label: "Instagram",
+    icon: InstagramIcon,
+    href: "https://instagram.com/pancakeswap_official",
+  },
+  {
+    label: "Github",
+    icon: GithubIcon,
+    href: "https://github.com/pancakeswap/",
+  },
   {
     label: "Discord",
     icon: DiscordIcon,
-    href: "https://discord.gg/zodiacswap",
+    href: "https://discord.gg/pancakeswap",
   },
   {
     label: "Medium",
     icon: MediumIcon,
     href: "https://medium.com/pancakeswap",
   },
-  // {
-  //   label: "Youtube",
-  //   icon: YoutubeIcon,
-  //   href: "https://www.youtube.com/@pancakeswap_official",
-  // },
+  {
+    label: "Youtube",
+    icon: YoutubeIcon,
+    href: "https://www.youtube.com/@pancakeswap_official",
+  },
 ];
 
 export const langs: Language[] = [...Array(20)].map((_, i) => ({
--- packages/uikit/src/widgets/Menu/components/footerConfig.ts
+++ packages/uikit/src/widgets/Menu/components/footerConfig.ts
@@ -6,25 +6,25 @@ export const footerLinks: (t: ContextApi["t"]) => FooterLinkType[] = (t) => [
     label: t("About"),
     items: [
       {
-        label: t("Documentation"),
-        href: "https://docs.zodiacswap.xyz/",
+        label: t("Contact"),
+        href: "https://docs.zodiacswap.xyz/contact-us",
         isHighlighted: true,
       },
-      // {
-      //   label: t("Brand"),
-      //   href: "https://docs.zodiacswap.xyz/brand",
-      // },
-      // {
-      //   label: t("Blog"),
-      //   href: "https://medium.com/pancakeswap",
-      // },
+      {
+        label: t("Brand"),
+        href: "https://docs.zodiacswap.xyz/brand",
+      },
+      {
+        label: t("Blog"),
+        href: "https://medium.com/pancakeswap",
+      },
       {
         label: t("Community"),
         href: "https://docs.zodiacswap.xyz/contact-us/telegram",
       },
       {
-        label: t("Medium"),
-        href: "https://medium.com/zodiacswap",
+        label: t("Litepaper"),
+        href: "https://v2litepaper.zodiacswap.xyz/",
       },
     ],
   },
@@ -35,39 +35,39 @@ export const footerLinks: (t: ContextApi["t"]) => FooterLinkType[] = (t) => [
         label: t("Customer Support"),
         href: "https://docs.zodiacswap.xyz/contact-us/customer-support",
       },
-      // {
-      //   label: t("Troubleshooting"),
-      //   href: "https://docs.zodiacswap.xyz/help/troubleshooting",
-      // },
+      {
+        label: t("Troubleshooting"),
+        href: "https://docs.zodiacswap.xyz/help/troubleshooting",
+      },
       {
         label: t("Guides"),
         href: "https://docs.zodiacswap.xyz/get-started",
       },
     ],
   },
-  // {
-  //   label: t("Developers"),
-  //   items: [
-  //     {
-  //       label: "Github",
-  //       href: "https://github.com/pancakeswap",
-  //     },
-  //     {
-  //       label: t("Documentation"),
-  //       href: "https://docs.zodiacswap.xyz",
-  //     },
-  //     {
-  //       label: t("Bug Bounty"),
-  //       href: "https://docs.zodiacswap.xyz/code/bug-bounty",
-  //     },
-  //     {
-  //       label: t("Audits"),
-  //       href: "https://docs.zodiacswap.xyz/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited",
-  //     },
-  //     {
-  //       label: t("Careers"),
-  //       href: "https://docs.zodiacswap.xyz/hiring/become-a-chef",
-  //     },
-  //   ],
-  // },
+  {
+    label: t("Developers"),
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/pancakeswap",
+      },
+      {
+        label: t("Documentation"),
+        href: "https://docs.zodiacswap.xyz",
+      },
+      {
+        label: t("Bug Bounty"),
+        href: "https://docs.zodiacswap.xyz/code/bug-bounty",
+      },
+      {
+        label: t("Audits"),
+        href: "https://docs.zodiacswap.xyz/help/faq#is-pancakeswap-safe-has-pancakeswap-been-audited",
+      },
+      {
+        label: t("Careers"),
+        href: "https://docs.zodiacswap.xyz/hiring/become-a-chef",
+      },
+    ],
+  },
 ];
