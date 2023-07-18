6f747ee1c9b75b92668d2b35ccd06cb43207a6db
Elvolution <75731970+Elvolution20@users.noreply.github.com>
2023-07-18 12:22:29 +0100

    Update On Footer and Change to Base Chain

--- apps/web/src/components/NetworkModal/PageNetworkSupportModal.tsx
+++ apps/web/src/components/NetworkModal/PageNetworkSupportModal.tsx
@@ -41,7 +41,7 @@ export function PageNetworkSupportModal() {
   return (
     <Modal title={title || t('Check your network')} hideCloseButton headerBackground="gradientCardHeader">
       <Grid style={{ gap: '16px' }} maxWidth="360px">
-        <Text bold>{t('It’s a BNB Smart Chain only feature')}</Text>
+        <Text bold>{t('It’s a Base Chain only feature')}</Text>
 
         {image && (
           <Box mx="auto" my="8px" position="relative" width="100%" minHeight="250px">
@@ -50,7 +50,7 @@ export function PageNetworkSupportModal() {
         )}
         <Text small>
           {t(
-            'Our Pools, Limit, Trading Competition, Prediction, Lottery and NFTs features are currently available only on BNB Chain! Come over and join the community in the fun!',
+            'Our Pools, Limit, Trading Competition, Prediction, Lottery and NFTs features are currently available only on Base Chain! Come over and join the community in the fun!',
           )}
         </Text>
         {canSwitch ? (
@@ -59,7 +59,7 @@ export function PageNetworkSupportModal() {
             isLoading={isLoading}
             onClick={() => (isWrongNetwork ? switchNetworkLocal(ChainId.BASE_GOERLI) : switchNetworkAsync(ChainId.BASE_GOERLI))}
           >
-            {t('Switch to %chain%', { chain: 'BNB Smart Chain' })}
+            {t('Switch to %chain%', { chain: 'Base Chain' })}
           </Button>
         ) : (
           <Message variant="danger">
