6af755ee8a0eedfea863a635bda1835b79bb5737
Elvolution <75731970+Elvolution20@users.noreply.github.com>
2023-11-17 21:05:01 +0100

    Update on LP

--- apps/web/src/views/RemoveLiquidity/index.tsx
+++ apps/web/src/views/RemoveLiquidity/index.tsx
@@ -174,7 +174,7 @@ export default function RemoveLiquidity({ currencyA, currencyB, currencyIdA, cur
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'Pancake LPs',
+      name: 'Zodiac LPs',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
