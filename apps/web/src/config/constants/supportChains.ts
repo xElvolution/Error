155ca05fdcb61048f6112c9a6255a1c2f8c07661
ahmadou5 <ahmadlasauwal@gmail.com>
2023-12-14 21:08:32 +0300

    'viction'

--- apps/web/src/config/constants/supportChains.ts
+++ apps/web/src/config/constants/supportChains.ts
@@ -1,6 +1,6 @@
 import { ChainId } from '@pancakeswap/sdk'
 
-export const SUPPORT_ONLY_BSC = [ChainId.BSC, ChainId.BASE_GOERLI]
+export const SUPPORT_ONLY_BSC = [ChainId.BSC, ChainId.VICTION_TESTNET, ChainId.BASE_GOERLI]
 export const SUPPORT_ONLY_VICTION = [ChainId.VICTION, ChainId.VICTION_TESTNET]
 export const SUPPORT_FARMS = [ChainId.BSC, ChainId.BSC_TESTNET, ChainId.ETHEREUM, ChainId.GOERLI, ChainId.BASE_GOERLI, ChainId.VICTION, ChainId.VICTION_TESTNET]
 
